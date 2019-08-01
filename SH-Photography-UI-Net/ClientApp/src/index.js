e8b5701b7938684708f43083b2b7e5c30de35e54
Stuart Hopwood <stoo.hopwood@gmail.com>
2019-08-01 14:23:22 +0100

    Add Service Worker to site

--- SH-Photography-UI-Net/ClientApp/src/index.js
+++ SH-Photography-UI-Net/ClientApp/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './_helpers';
 import { App } from './App/index.js';
+import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap'; // Import Bootstraps JS Scripts
 import './global.css';
 
 // setup fake backend
 //import { configureFakeBackend } from './_helpers';
 //configureFakeBackend();
+
+registerServiceWorker();
 
 ReactDOM.render(
   <Provider store={store}>
