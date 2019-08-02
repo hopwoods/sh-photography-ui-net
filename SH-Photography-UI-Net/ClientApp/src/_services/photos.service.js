a58fd767b3f09c1ae60a10f4f8ed3396189eb0c9
Stuart Hopwood <stoo.hopwood@gmail.com>
2019-08-02 09:14:36 +0100

    Added hook for getting viewport dimensions to pass to API.

--- SH-Photography-UI-Net/ClientApp/src/_services/photos.service.js
+++ SH-Photography-UI-Net/ClientApp/src/_services/photos.service.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
-import Api from "./../_helpers/api";
+import Api from './../_helpers/api';
 
-export const GetPhotoAlbum = function(goolgeAlbumId) {
-  return Api.get(`/photos/getalbumphotos?albumId=${goolgeAlbumId}`).then(
-    Response => {
-      return Response.data.photos;
+export const GetPhotoAlbum = function(googleAlbumId, viewportWidth, viewportHeight) {
+  return Api.get(`/photos/getalbumphotos?albumId=${googleAlbumId}&viewportWidth=${viewportWidth}&viewportHeight=${
+    viewportHeight}`).then(
+    response => {
+      return response.data.photos;
     }
   );
 };
